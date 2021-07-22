90f5f6f8fb92c3bb5b0a3fdbe795b6bbf69d4fe2
Stephanie Correa <stephanie.correa24@gmail.com>
2021-07-21 21:23:20 -0300

    function list of posts created

---
+++ ApiBlog/Controllers/PostController.cs
@@ -0,0 +1,31 @@
+﻿using ApiBlog.Models;
+using ApiBlog.Views;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiBlog.Controllers
+{
+    [Route("api/posts")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+
+        private BlogContext _context;
+
+        public PostController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PostDTO>>> GetPosts()
+        {
+            return await _context.Posts
+                                 .Select(p => new PostDTO { Id = p.Id, Title = p.Title, Category = p.Category, Image = p.Image, Date = p.Date }).OrderByDescending(p => p.Date).ToListAsync();
+        }
+
+    }
+}
